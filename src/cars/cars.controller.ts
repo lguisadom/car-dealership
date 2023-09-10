c56ea78bd4f70f8c19856ed60f94a2483c1c82a7
Luis Guisado <lguisadom@gmail.com>
2023-09-10 17:32:00 -0500

    feat: Post, Patch and Delete http handler methods

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,9 +1,18 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { CarsService } from './cars.service';
 
 @Controller('cars')
 export class CarsController {
-  constructor(private readonly carsService: CarsService) { }
+  constructor(private readonly carsService: CarsService) {}
 
   @Get()
   getAllCars() {
@@ -14,5 +23,23 @@ export class CarsController {
   getById(@Param('id', ParseIntPipe) id: number) {
     console.log({ id });
     return this.carsService.findOneById(+id);
+  }
+
+  @Post()
+  createCar(@Body() body: any) {
+    return body;
+  }
+
+  @Patch(':id')
+  updateCar(@Param('id', ParseIntPipe) id: number, @Body() body: any) {
+    return body;
+  }
+
+  @Delete(':id')
+  deleteCar(@Param('id', ParseIntPipe) id: number) {
+    return {
+      method: 'DELETE',
+      id,
+    };
   }
 }
